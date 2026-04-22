fc8410f76f5ae6901bf5919f04c4f8cd3e7f98b5
SilvenaS <silvena.shivacheva@aerlingus.com>
2026-04-22 16:21:56 +0300

    initial route

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -5,4 +5,6 @@ import { DEFAULT_LOCALE, LOCALES } from "~/constants";
 export const routing = defineRouting({
   locales: LOCALES,
   defaultLocale: DEFAULT_LOCALE,
+  // Always redirect `/` to default locale (`/bg`) instead of browser language.
+  localeDetection: false,
 });
